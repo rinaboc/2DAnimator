0fd68779c414ee4e1eda3c801f0235c0c14c1b17
rinaboc <bocska.karina@gmail.com>
2026-05-19 17:01:41 +0200

    add vertex on edge

--- Assets/Scripts/Utility/Mesh/CDT.cs
+++ Assets/Scripts/Utility/Mesh/CDT.cs
@@ -1,3 +1,4 @@
+using System;
 using Assets.Scripts.Data.MeshInfo;
 using UnityEngine;
 
@@ -14,9 +15,23 @@ namespace Assets.Scripts.Utility.Mesh
 
         public MeshInfo AddVertex(Vector2 point)
         {
+            HalfEdge e = FindEdgeContaining(point);
+            if (e != null)
+            {
+                AddVertexOnEdge(point, e);
+                return _mesh;
+            }
+
             Face f0 = FindFaceContaining(point);
             if (f0 == null) return null;
 
+            AddVertexOnFace(point, f0);
+
+            return _mesh;
+        }
+
+        private void AddVertexOnFace(Vector2 point, Face f0)
+        {
             Vertex vNew = new() { Position = point, UV = Vector2.zero };
             _mesh.Vertices.Add(vNew);
 
@@ -88,13 +103,147 @@ namespace Assets.Scripts.Utility.Mesh
             LegalizeEdge(e0, vNew);
             LegalizeEdge(e1, vNew);
             LegalizeEdge(e2, vNew);
+        }
 
-            return _mesh;
+        private void AddVertexOnEdge(Vector2 point, HalfEdge edgeToSplit)
+        {
+            bool constrained = edgeToSplit.IsConstrained;
+
+            HalfEdge e_twin = edgeToSplit.Twin;
+            HalfEdge e_AC = edgeToSplit.Next;
+            HalfEdge e_CB = e_AC.Next;
+            HalfEdge e_BD = e_twin?.Next;
+            HalfEdge e_DA = e_BD?.Next;
+
+            Vertex vA = edgeToSplit.Next.Origin;
+            Vertex vB = edgeToSplit.Origin;
+            Vertex vC = edgeToSplit.Next.Next.Origin;
+            Vertex vD = e_twin?.Next.Next.Origin;
+
+            Vertex vNew = new() { Position = point, UV = Vector2.zero };
+            _mesh.Vertices.Add(vNew);
+
+            Face fA = new();
+            Face fD = edgeToSplit.Face;
+            Face fC = e_twin?.Face;
+            Face fB = e_twin == null ? null : new();
+
+            _mesh.Faces.Add(fA);
+            if (fB != null) _mesh.Faces.Add(fB);
+
+            HalfEdge vNew_vC = new();
+            HalfEdge vC_VNew = new();
+            HalfEdge vNew_vD = e_twin == null ? null : new();
+            HalfEdge vD_vNew = e_twin == null ? null : new();
+            HalfEdge vA_vNew = e_twin == null ? null : new();
+            HalfEdge vB_vNew = new();
+
+            _mesh.HalfEdges.AddRange(new HalfEdge[] { vNew_vC, vC_VNew, vB_vNew });
+            if (e_twin != null)
+            {
+                _mesh.HalfEdges.AddRange(new HalfEdge[] { vNew_vD, vD_vNew, vA_vNew });
+
+                vA_vNew.Origin = vA;
+                vA_vNew.Twin = edgeToSplit;
+                vA_vNew.Face = fC;
+                vA_vNew.Next = vNew_vD;
+                vA_vNew.IsConstrained = constrained;
+
+                e_twin.Origin = vNew;
+                e_twin.Twin = vB_vNew;
+                e_twin.Face = fB;
+
+                vNew_vD.Origin = vNew;
+                vNew_vD.Twin = vD_vNew;
+                vNew_vD.Face = fC;
+                vNew_vD.Next = e_DA;
+
+                vD_vNew.Origin = vD;
+                vD_vNew.Twin = vNew_vD;
+                vD_vNew.Face = fB;
+                vD_vNew.Next = e_twin;
+
+                e_BD.Face = fB;
+                e_BD.Next = vD_vNew;
+
+                e_DA.Face = fC;
+                e_DA.Next = vA_vNew;
+
+                fC.Edge = e_DA;
+                fB.Edge = e_BD;
+
+                vD.IncidentEdge = e_DA;
+            }
+
+            edgeToSplit.Origin = vNew;
+            edgeToSplit.Face = fD;
+            edgeToSplit.Twin = e_twin != null ? vA_vNew : null;
+
+            vB_vNew.Origin = vB;
+            vB_vNew.Twin = e_twin;
+            vB_vNew.Face = fA;
+            vB_vNew.Next = vNew_vC;
+            vB_vNew.IsConstrained = constrained;
+
+            vNew_vC.Origin = vNew;
+            vNew_vC.Twin = vC_VNew;
+            vNew_vC.Face = fA;
+            vNew_vC.Next = e_CB;
+
+            vC_VNew.Origin = vC;
+            vC_VNew.Next = edgeToSplit;
+            vC_VNew.Face = fD;
+            vC_VNew.Twin = vNew_vC;
+
+            e_AC.Face = fD;
+            e_AC.Next = vC_VNew;
+
+            e_CB.Face = fA;
+            e_CB.Next = vB_vNew;
+
+            fD.Edge = e_AC;
+            fA.Edge = e_CB;
+
+            vA.IncidentEdge = e_AC;
+            vC.IncidentEdge = e_CB;
+            vB.IncidentEdge = e_twin != null ? e_BD : vB_vNew;
+
+            vNew.IncidentEdge = vNew_vC;
+
+            LegalizeEdge(e_AC, vNew);
+            LegalizeEdge(e_CB, vNew);
+
+            if (e_twin != null)
+            {
+                LegalizeEdge(e_BD, vNew);
+                LegalizeEdge(e_DA, vNew);
+            }
         }
 
         public MeshInfo RemoveVertex(Vertex v)
         {
             throw new System.NotImplementedException();
+        }
+
+        private HalfEdge FindEdgeContaining(Vector2 p)
+        {
+            foreach (var edge in _mesh.HalfEdges)
+            {
+                Vector2 ep = edge.Origin.Position;
+                Vector2 enp = edge.Next.Origin.Position;
+
+                float d = Geometry.Orientation(ep, enp, p);
+                if (Math.Abs(d) > 0.1) continue;
+
+                if (Math.Min(ep.x, enp.x) - 0.1f < p.x && Math.Max(ep.x, enp.x) + 0.1f > p.x &&
+                    Math.Min(ep.y, enp.y) - 0.1f < p.y && Math.Max(ep.y, enp.y) + 0.1f > p.y
+                )
+                {
+                    return edge;
+                }
+            }
+
+            return null;
         }
 
         private Face FindFaceContaining(Vector2 p)
